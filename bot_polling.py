480da0163b24aabfab9eb5567f7e8cb2734f1ec1
AkasakaID <fawwazthoerif@gmail.com>
2022-09-16 09:10:14 +0700

    just an update

--- bot_polling.py
+++ bot_polling.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from requests import get,post
+from requests import get, post
 from system import Bot
 from config import *
 
@@ -10,19 +10,20 @@ update_id = 0
 print("BOT ACTIVED")
 print("PRESS CTRL + C TO EXIT ")
 while True:
-	try:
-		req = get(f"https://api.telegram.org/bot{token_bot}/getupdates",params={"offset":update_id}).json()
-		if len(req['result']) == 0:
-			continue
-		try:
-			update = req["result"][0]
+    try:
+        req = get(
+            f"https://api.telegram.org/bot{token_bot}/getupdates", params={"offset": update_id}).json()
+        if len(req['result']) == 0:
+            continue
+        try:
+            update = req["result"][0]
 #			for update in update:
-			Bot(update)
-			update_id = update['update_id'] + 1
-			print("-"*40)
-		except KeyError:
-			continue
-	except KeyboardInterrupt:
-		exit()
-	except requests.exceptions.ConnectionError:
-		continue
+            Bot(update)
+            update_id = update['update_id'] + 1
+            print("-"*40)
+        except KeyError:
+            continue
+    except KeyboardInterrupt:
+        exit()
+    except requests.exceptions.ConnectionError:
+        continue
--- config-dist.py
+++
@@ -1,3 +0,0 @@
-
-# token bot from botfather
-token_bot = ""
